5da9ee9978b515f871cd2c6efac2464445933766
gamezzz121 <gamezzz121@gmail.com>
2022-03-07 21:06:00 +0700

    ...
    ....

--- ของจริง/main.py
+++ ของจริง/main.py
@@ -4,12 +4,14 @@ import numpy as np
 import mapper
 
 img = cv2.imread('test1.jpg')
-imS = cv2.resize(img, (500, 470))
-
+imS= cv2.resize(img, (500, 470))
+cv2.imwrite('output/0img.jpg',imS)
 gray = cv2.cvtColor(imS, cv2.COLOR_BGR2GRAY)
+cv2.imwrite('output/1pre_gray.jpg',gray)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+cv2.imwrite('output/2pre_blurred.jpg',blurred)
 thresh = cv2.threshold(blurred, 150, 255, cv2.THRESH_BINARY)[1]
-
+cv2.imwrite('output/3pre_thresh.jpg',thresh)
 
 # corners = cv2.goodFeaturesToTrack(thresh, 4 ,0.01,100)
 # corners = np.int0(corners)
@@ -20,6 +22,7 @@ thresh = cv2.threshold(blurred, 150, 255, cv2.THRESH_BINARY)[1]
 edged = cv2.Canny(thresh, 50, 100)
 edged = cv2.dilate(edged, None, iterations=1)
 edged = cv2.erode(edged, None, iterations=1)
+cv2.imwrite('output/4pre_edged.jpg',edged)
 
 cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
@@ -27,10 +30,10 @@ cnts = imutils.grab_contours(cnts)
 for c in cnts:
     epsilon = 0.06 * cv2.arcLength(c, True)
     approx = cv2.approxPolyDP(c, epsilon, True)
-    # cv2.drawContours(thresh, [approx], 0, (255, 255, 255), 2)
+    # contourSS = cv2.drawContours(imS, [approx], 0, (0, 255,0), 2)
 
     print(approx)
-
+# cv2.imwrite('output/5contour.jpg',contourSS)
 
 # for c in cnts: #1
 #  # find bounding box coordinates
@@ -58,7 +61,8 @@ pts1 = np.float32([myPoints[2], myPoints[4], myPoints[1], myPoints[0]])
 pts2 = np.float32([[0, 0], [w, 0], [0, h], [w, h]])
 matrix = cv2.getPerspectiveTransform(pts1, pts2)
 result = cv2.warpPerspective(imS, matrix, (w, h))
-cv2.imwrite('result.jpg',result)
+cv2.imwrite('output/6_perspective.jpg',result)
+
 
 
 gray1 = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
@@ -78,7 +82,7 @@ for c in cnts:
     cv2.rectangle(result, (x, y), (x + w1, y + h1), (0, 255, 0), 10)
 
 
-cv2.imwrite('resultFeet.jpg',result)
+cv2.imwrite('output/7edgedFeet.jpg',result)
 print(x)
 print(y)
 print(w1)
