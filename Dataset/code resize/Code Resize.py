97485009a5878cefccbfba714ebd9450c28537a0
gamezzz121 <gamezzz121@gmail.com>
2022-05-18 20:11:48 +0700

    add img footA4

--- Dataset/code resize/Code Resize.py
+++ Dataset/code resize/Code Resize.py
@@ -1,7 +1,7 @@
 import os
 import cv2
 
-path = 'D:/GitHub/projectsonteen/Dataset/imgFootBan/'
+path = 'D:/GitHub/projectsonteen/Dataset/imgFootA4/'
 dir = os.listdir(path)
 # print(path)
 # print(len(dir))
@@ -12,7 +12,7 @@ for i in dir:
     img = cv2.imread(path + i)
     print(img)
     img = cv2.resize(img,(500,500))
-    cv2.imwrite("D:/GitHub/projectsonteen/Dataset/For train/type foot/images/"+i,img)
+    cv2.imwrite("D:/GitHub/projectsonteen/Dataset/For train/detectA4/images/"+i,img)
 
 # cv2.imshow('image',img)
 # cv2.waitKey(0)
