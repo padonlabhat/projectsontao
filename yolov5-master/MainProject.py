8462a56368308ee35d6b93fe975be4ca1706cabc
gamezzz121 <gamezzz121@gmail.com>
2022-05-23 14:27:36 +0700

    update value

--- yolov5-master/MainProject.py
+++ yolov5-master/MainProject.py
@@ -4,15 +4,17 @@ import foot_size
 import shoe_size
 # rtsp://192.168.1.101:8080/h264_pcm.sdpi
 camera = 3
-detect.detect_by_video(camera,'D:/GitHub/projectsonteen/my models/best_typeFoot.pt')
+pathTypefoot = 'my models/best_typeFoot.pt'
+pathDetectA4 = 'my models/best_footA4.pt'
+detect.detect_by_video(camera,pathTypefoot)
 # detect.detect_by_img('D:/GitHub/projectsonteen/Dataset/For train/type foot/images/foot (99).jpg'
-#                    ,'D:/GitHub/projectsonteen/my models/best_typeFoot.pt')
-# detect.detect_by_video('rtsp://192.168.1.120:8080/h264_pcm.sdp','D:/GitHub/projectsonteen/my models/best_footA4.pt')
+#                    ,pathDetectA4)
+# detect.detect_by_video(camera,pathDetectA4)
 
-foot_size.perspectiveA4('input/test1.jpg')
-footSize = foot_size.findSize('output/result_A4.jpg')
-shoe_size.showsize(footSize)
+# foot_size.perspectiveA4('input/test1.jpg')
+# footSize = foot_size.findSize('output/result_A4.jpg')
+# shoe_size.showsize(footSize)
 
-# shoe_size.show()
+
 
 
